9e486d2de17dc7791be8f188bb28d24c13e1181b
Cade Scroggins <git@cade.me>
2020-12-14 00:40:14 +0530

    update focus styles

--- c/AuikContent.js
+++ c/AuikContent.js
@@ -2,13 +2,23 @@ import GoogleFonts from 'next-google-fonts';
 import React from 'react';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { DefaultSeo } from 'next-seo';
+import { Global } from '@emotion/react';
 import { useRouter } from 'next/router';
+import 'focus-visible/dist/focus-visible';
 
 const AuikContent = ({ children, fonts, seo, theme }) => {
   const router = useRouter();
 
   return (
     <ChakraProvider resetCSS theme={extendTheme(theme)}>
+      <Global
+        styles={{
+          '.js-focus-visible :focus:not([data-focus-visible-added])': {
+            boxShadow: 'none',
+            outline: 'none',
+          },
+        }}
+      />
       {fonts && <GoogleFonts href={fonts} />}
       <DefaultSeo
         titleTemplate={router.asPath === '/' ? undefined : seo.titleTemplate}
