a6ee59ca773a84351e89813bedf24aae935a5b2c
Cade Scroggins <git@cade.me>
2020-12-13 18:32:26 +0530

    update title template

--- c/AuikContent.js
+++ c/AuikContent.js
@@ -2,14 +2,22 @@ import GoogleFonts from 'next-google-fonts';
 import React from 'react';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { DefaultSeo } from 'next-seo';
+import { useRouter } from 'next/router';
 
-const AuikContent = ({ children, fonts, seo, theme }) => (
-  <ChakraProvider resetCSS theme={extendTheme(theme)}>
-    {fonts && <GoogleFonts href={fonts} />}
-    <DefaultSeo {...seo} />
-    {children}
-  </ChakraProvider>
-);
+const AuikContent = ({ children, fonts, seo, theme }) => {
+  const router = useRouter();
+
+  return (
+    <ChakraProvider resetCSS theme={extendTheme(theme)}>
+      {fonts && <GoogleFonts href={fonts} />}
+      <DefaultSeo
+        titleTemplate={router.asPath === '/' ? undefined : seo.titleTemplate}
+        {...seo}
+      />
+      {children}
+    </ChakraProvider>
+  );
+};
 
 AuikContent.defaultProps = {
   children: null,
