01a7088c8a1da7528d7e73a4067cf15b838005f6
Cade Scroggins <git@cade.me>
2021-03-12 13:52:19 -0800

    remove input mask

--- c/PhoneInput.js
+++ c/PhoneInput.js
@@ -1,21 +1,10 @@
 import * as C from '@chakra-ui/react';
-import InputMask from 'react-input-mask';
 import React from 'react';
 
 const PhoneInput = (props, ref) => (
   <C.InputGroup>
     <C.InputLeftAddon bg="gray.200">+1</C.InputLeftAddon>
-    <C.Input
-      ref={ref}
-      alwaysShowMask={false}
-      as={InputMask}
-      formatChars={{ 0: '[0-9]' }}
-      mask="(000) 000-0000"
-      maskChar=" "
-      placeholder="(012) 345-6789"
-      type="tel"
-      {...props}
-    />
+    <C.Input ref={ref} placeholder="(012) 345-6789" type="tel" {...props} />
   </C.InputGroup>
 );
 
